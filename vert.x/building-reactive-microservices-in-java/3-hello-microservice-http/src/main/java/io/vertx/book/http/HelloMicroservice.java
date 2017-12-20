b049a0b48219c7c55cdf0876c8c3233fe8f1ed21
Falk Lange <falk.lange@gmail.com>
2017-12-20 11:26:18 +0100

    add microservice project with routing

---
+++ vert.x/building-reactive-microservices-in-java/3-hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -0,0 +1,16 @@
+package io.vertx.book.http;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    Router router = Router.router(vertx);
+    router.get("/").handler(rc -> rc.response().end("hello"));
+    router.get("/:name").handler(rc -> rc.response().end("hello " + rc.pathParam("name")));
+
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
+}
