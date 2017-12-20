18529f72c87fa36d4e7f66ccef6df30b279fb6d0
Falk Lange <falk.lange@gmail.com>
2017-12-20 12:26:07 +0100

    Producing JSON

--- vert.x/building-reactive-microservices-in-java/3-hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ vert.x/building-reactive-microservices-in-java/3-hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,7 +1,10 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class HelloMicroservice extends AbstractVerticle {
 
@@ -10,7 +13,17 @@ public class HelloMicroservice extends AbstractVerticle {
     Router router = Router.router(vertx);
     router.get("/").handler(rc -> rc.response().end("hello"));
     router.get("/:name").handler(rc -> rc.response().end("hello " + rc.pathParam("name")));
+    router.get("/json/:name").handler(this::hello);
 
     vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
+
+  private void hello(RoutingContext rc) {
+    String message = "hello";
+    if (rc.pathParam("name") != null) {
+      message += " " + rc.pathParam("name");
+    }
+    JsonObject json = new JsonObject().put("message", message);
+    rc.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json").end(json.encode());
   }
 }
